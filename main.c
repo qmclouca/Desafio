a3d7f898810b51e61ad213d6f39ad8ab960abdb2
rodolfo.bortoluzzi <rodolfo.bortoluzzi@bit.com.br>
2022-11-18 23:16:24 -0300

    all working task 11

--- main.c
+++ main.c
@@ -1,7 +1,35 @@
 #include <string.h>
 #include <stdio.h>
-#include "searchPattern.h"
-#include "substringSearch.h"
+
+int matchPattern(char text[], char pattern[]){
+    int c, d, e, text_length, pattern_length, position = -1;
+
+    text_length    = strlen(text);
+    pattern_length = strlen(pattern);
+
+    if (pattern_length > text_length) {
+        return -1;
+    }
+
+    for (c = 0; c <= text_length - pattern_length; c++) {
+        position = e = c;
+
+        for (d = 0; d < pattern_length; d++) {
+            if (pattern[d] == text[e]) {
+                e++;
+            }
+            else {
+                break;
+            }
+        }
+        if (d == pattern_length) {
+            return position;
+        }
+    }
+    return -1;
+}
+
+
 
 int main(int argc, char* argv[]) {
     char path[255];
@@ -20,10 +48,10 @@ int main(int argc, char* argv[]) {
     strcpy(tag, argv[1]);
 
     for (count=0; count+3<argc; count++){
-        memset(path,0,sizeof (path));
+        memset(path,0,sizeof (path)); //zerar
         strcat(path,argv[2]);
         strcat(path,"\\");
-        strcat(path,argv[count+3]);
+        strcat(path,argv[count+3]); // file name with path
         FILE* file = fopen(path, "r");
         if(!file){
             printf("\n Unable to open: %s ", path);
@@ -31,13 +59,60 @@ int main(int argc, char* argv[]) {
         }
 
         char line[255];
-        printf("arquivo %s: \n", argv[count+3]);
+        char *lineTag;
+        printf("\n arquivo %s: \n", argv[count+3]);
 
         while (fgets(line, sizeof (line), file)){
-            position = matchPattern(line, tag);
-            if (position != -1){
-                printf(line);
-                position = -1;
+            char lineToPrint[255];
+            strcpy(lineToPrint, line);
+            lineTag = strtok(line, ";");
+            position = -1;
+            int tagLength = 0;
+            tagLength = strlen(tag);
+
+            if (tag[0]==(char)42){  //É UM PADRÃO
+                char endTag[tagLength-1];
+                memcpy(endTag, &tag[1],tagLength);
+                position = matchPattern(endTag, "*"); //TEM DOIS *
+                if (position != -1){ //ENCONTRA O PADRÃO EM QUALQUER LUGAR DA TAGLINE
+                    int tagLength = strlen(tag);
+                    char subTag[tagLength-2];
+                    char *buff = tag;
+                    memcpy(subTag, &buff[1],tagLength-1);
+
+                    if (matchPattern(lineTag, subTag) != -1){
+                        printf("%s\n ", lineToPrint); //imprime padrões em qualquer lugar da tag
+                        position = -1;
+                    }
+                } else {  //SÓ TEM * NA FRENTE
+                    int endTagLength = strlen(endTag);
+                    int lineTagLength = strlen(lineTag);
+                    char *buff = lineTag;
+                    char subTag[endTagLength];
+                    memcpy(subTag, &buff[lineTagLength-endTagLength],lineTagLength);
+                    int comp = strcmp(subTag,endTag);
+                    if (comp == 0) {
+                        printf("%s\n ", lineToPrint); //imprime padrões de fim de tag
+                    }
+                }
+            } else {
+                if (tag[tagLength-1] == (char)42){
+                    char *subTag = strtok(tag, "*");
+                    int subTagLength = strlen(subTag);
+
+                    char *buff = lineTag;
+                    char subLineTag[subTagLength];
+                    memcpy(subLineTag, &buff[0],subTagLength);
+                    int comp = strcmp(subTag,subLineTag);
+                    if (comp == 0){
+                        printf("%s\n", lineToPrint); //imprime padrões no início da tag
+                    }
+                } else { //Se nenhum * na tag
+                    position = matchPattern(lineTag, tag);
+                    if (position != -1){
+                        printf("%s, ", lineToPrint); //imprime tags sem padrões
+                    }
+                }
             }
         }
         fclose(file);
